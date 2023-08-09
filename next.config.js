1e7c5855883d013c8012051d256be3ef65e5a5f0
Ayushi Dwivedi <97382798+damncode12@users.noreply.github.com>
2023-08-09 23:38:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       'avatars.githubusercontent.com',
       'imgur.com',
       'reprodev.com',
-      'devfolio-prod',
+      'devfolio-prod.s3.ap-south-1.amazonaws.com',
       'static.javatpoint.com',
       'cdn2.vectorstock.com'
     ],
